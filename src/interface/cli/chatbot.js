5516fa19bef62c03e8afadb1fcce0fc43717534c
Felipe Moura <felipe.moura@implantatecnologia.com.br>
2025-05-08 19:33:23 -0300

    feat: add question to user chose if he wants check weather or exit

--- src/interface/cli/chatbot.js
+++ src/interface/cli/chatbot.js
@@ -1,9 +1,18 @@
+import readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
+
 export class Chatbot {
   constructor() {
     this.state = 'start';
+    this.rl = readline.createInterface({ input, output });
   }
 
   async start() {
-    console.log(this.start);
+    console.log('👋 Olá! Bem-vindo ao Chatbot do Clima!');
+    console.log('\nO que você deseja fazer?');
+    console.log('[1] Consultar clima');
+    console.log('[2] Sair');
+    const option = await this.rl.question('Escolha uma opção: ');
+    return option;
   }
 }
