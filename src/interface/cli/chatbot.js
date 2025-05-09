9e9363ee1f37309e1669d9940a665c49376c43c4
Felipe Moura <felipe.moura@implantatecnologia.com.br>
2025-05-08 21:33:30 -0300

    refactor: create methods to build menu and consult Weather

--- src/interface/cli/chatbot.js
+++ src/interface/cli/chatbot.js
@@ -9,47 +9,11 @@ export class Chatbot {
   }
 
   async start() {
-    console.log('👋 Olá! Bem-vindo ao Chatbot do Clima!');
-    console.log('\nO que você deseja fazer?');
-    console.log('[1] Consultar clima');
-    console.log('[2] Sair');
-    const option = await this.rl.question('Escolha uma opção: ');
+    const option = await this.buildMenu();
 
     if (option === '1') {
       // Perguntar a cidade
-      const city = await this.rl.question('Digite o nome da cidade: ');
-
-      //Buscar coordernadas
-
-      const response = await axios.get(
-        'https://nominatim.openstreetmap.org/search',
-        {
-          params: {
-            q: city,
-            format: 'json',
-            limit: 1
-          },
-          headers: { 'User-Agent': 'chatbot-clima' }
-        }
-      );
-
-      if (!response.data[0]) {
-        throw new Error('Cidade não encontra');
-      }
-
-      const { lat, lon } = response.data[0];
-
-      //Buscar temperatura
-      const r = await axios.get('https://api.open-meteo.com/v1/forecast', {
-        params: {
-          latitude: lat,
-          longitude: lon,
-          current_weather: true
-        }
-      });
-      const temperature = r.data.current_weather?.temperature;
-
-      console.log(temperature);
+      await this.consultWeather();
     } else if (option === '2') {
       console.log('Ate mais');
       this.rl.close();
@@ -58,5 +22,46 @@ export class Chatbot {
       console.log('Opção Invalida');
     }
     return option;
+  }
+
+  async buildMenu() {
+    console.log('👋 Olá! Bem-vindo ao Chatbot do Clima!');
+    console.log('\nO que você deseja fazer?');
+    console.log('[1] Consultar clima');
+    console.log('[2] Sair');
+    return await this.rl.question('Escolha uma opção: ');
+  }
+
+  async consultWeather() {
+    const city = await this.rl.question('Digite o nome da cidade: ');
+    const response = await axios.get(
+      'https://nominatim.openstreetmap.org/search',
+      {
+        params: {
+          q: city,
+          format: 'json',
+          limit: 1
+        },
+        headers: { 'User-Agent': 'chatbot-clima' }
+      }
+    );
+
+    if (!response.data[0]) {
+      throw new Error('Cidade não encontra');
+    }
+
+    const { lat, lon } = response.data[0];
+
+    //Buscar temperatura
+    const r = await axios.get('https://api.open-meteo.com/v1/forecast', {
+      params: {
+        latitude: lat,
+        longitude: lon,
+        current_weather: true
+      }
+    });
+    const temperature = r.data.current_weather?.temperature;
+
+    console.log(`A temperatura em ${city} é de ${temperature} °C`);
   }
 }
