e252cbadb5d712edc7913f5bef235a459e5c238f
Felipe Moura <felipe.moura@implantatecnologia.com.br>
2025-05-12 07:21:56 -0300

    feat: configure pino

--- src/shared/logger.js
+++ src/shared/logger.js
@@ -1,10 +1,9 @@
+import pinoPretty from 'pino-pretty';
 import pino from 'pino';
-export const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:standard'
-    }
-  }
+const stream = pinoPretty({
+  colorize: true,
+  translateTime: 'SYS:standard',
+  ignore: 'pid,hostname'
 });
+
+export const logger = pino(stream);
